5708fdce99333dc2cdac386a7ce8207ea257a2ba
Libor <Szucs>
2024-01-18 17:57:04 +0100

    Nová třída FileHandler s metody pro čtení a zápis do souboru

---
+++ TextEditor/FileHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextEditor
+{
+    internal class FileHandler
+    {
+        /// <summary>
+        /// Reads text from file
+        /// </summary>
+        /// <param name="importFileName">Name of imported file</param>
+        /// <returns></returns>
+        public string Read(string importFileName)
+        {
+            using (StreamReader sr = new StreamReader(importFileName))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+
+        /// <summary>
+        /// Writes text to file
+        /// </summary>
+        /// <param name="exportFileName">Name of exported file</param>
+        /// <param name="text">Text to write</param>
+        /// <returns></returns>
+        public void Write(string exportFileName, string text)
+        {
+            using (StreamWriter sw = new StreamWriter(exportFileName))
+            {
+                sw.Write(text);
+            }
+        }
+    }
+}
